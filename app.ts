1c738618343a6ab5b8b8eb1dd9497db464ac84fc
Stanislav Eliseev <s.eliseev@dexpa.io>
2023-05-30 16:47:05 +0200

    add keystore to example

--- app.ts
+++ app.ts
@@ -1,7 +1,28 @@
-import { Address, ProviderRpcClient } from 'everscale-inpage-provider';
-import { EverscaleStandaloneClient } from 'everscale-standalone-client/nodejs';
+import {Address, ProviderRpcClient} from 'everscale-inpage-provider';
+import {
+    EverWalletAccount,
+    Clock,
+    EverscaleStandaloneClient,
+    SimpleAccountsStorage,
+    SimpleKeystore
+} from 'everscale-standalone-client/nodejs';
+import {deriveBip39Phrase, makeBip39Path} from "everscale-crypto";
 
 // @ts-ignore
+
+let keystore = new SimpleKeystore();
+let path = makeBip39Path(0);
+let keys = deriveBip39Phrase('', path);
+keystore.addKeyPair('giver', keys );
+
+let address = new Address('0:wallet');
+
+let accountStorage = new SimpleAccountsStorage();
+accountStorage.addAccount(new EverWalletAccount(address));
+accountStorage.defaultAccount = address;
+let clock = new Clock();
+
+
 const ever = new ProviderRpcClient({
     fallback: () =>
         EverscaleStandaloneClient.create({
@@ -10,9 +31,12 @@ const ever = new ProviderRpcClient({
                 type: 'jrpc',
                 data: {
                     // create your own project at https://dashboard.evercloud.dev
-                    endpoint: 'https://jrpc.path',
+                    endpoint: 'http://jrpc.path',
                 },
             },
+            keystore: keystore,
+            accountsStorage: accountStorage,
+            clock: clock
         }),
 });
 
@@ -32,6 +56,26 @@ const ABI = {
     data: [],
 } as const;
 
+const WalletAbi = {
+    "ABI version": 2,
+    "version": "2.3",
+    "header": ["pubkey", "time", "expire"],
+    "functions": [
+        {
+            "name": "sendTransaction",
+            "inputs": [
+                {"name": "dest", "type": "address"},
+                {"name": "value", "type": "uint128"},
+                {"name": "bounce", "type": "bool"},
+                {"name": "flags", "type": "uint8"},
+                {"name": "payload", "type": "cell"}
+            ],
+            "outputs": []
+        }
+    ],
+    "events": [],
+} as const;
+
 async function start(): Promise<void> {
     await ever.ensureInitialized();
 
@@ -47,10 +91,39 @@ async function start(): Promise<void> {
         withoutSignature: true,
     });
 
-    console.log("hello");
     const subscriber = new ever.Subscriber();
     await subscriber.trace(transaction).finished();
 }
+async function startWallet(): Promise<void> {
+    await ever.ensureInitialized();
+
+    await ever.requestPermissions({
+        permissions: ['basic', 'accountInteraction'],
+    });
 
 
-start().then(() => console.log("done")).catch((x) => console.log(x));
+
+    const wallet = new ever.Contract(WalletAbi, address);
+
+    //const walletState = await ever.getFullContractState({address: wallet.address});
+    //const stateInit = !walletState?.state.isDeployed ? stateInit : undefined;
+
+    const {transaction} = await wallet.methods
+        .sendTransaction({
+            dest: new Address('0:wallet'),
+            value: '1000000000', // amount in nano EVER
+            bounce: false,
+            flags: 3,
+            payload: ''
+        }).sendExternal({
+            publicKey: 'pubkey',
+            stateInit: undefined,
+        });
+
+    console.log(transaction);
+}
+
+
+
+start().then(() => console.log("done start")).catch((x) => console.log(x));
+startWallet().then(() => console.log("done startWallet")).catch((x) => console.log(x));
